1a4b89d7d74dc0987a62da82c52640eff40debdd
jevk <jevk@users.noreply.github.com>
2020-04-23 16:52:15 +0300

    Big change
    Removed a ton of files and moved lines to a separate file.
    +Southern Europe and Northern African roads

--- howDoIGo.js
+++ howDoIGo.js
@@ -1,9 +1,27 @@
+
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    if (hours != 0)  {hours += ":"} else {hours = ""}
+    if (minutes != 0)  {minutes += ":"} else {minutes = ""}
+    return hours+minutes+seconds;
+}
+
 function how_do_i_go() {
     clear_path()
     var from = document.getElementById("from").value
     var to   = document.getElementById("to"  ).value
-    var l = pathfind(citiesL[from], citiesL[to])
-    console.log(l)
+    window.path_buff = []
+    pathfind(citiesL[to], citiesL[from])
+
+    l = window.path_buff
     if (l == false) {
         document.getElementById("result").innerText = "Path not found"
         document.getElementById("result").style.display = "block"
@@ -13,50 +31,58 @@ function how_do_i_go() {
         document.getElementById("result").innerText = ""
         document.getElementById("result").style.display = "grid"
     }
-    l.unshift([l[0][0], "foot"])
     fracs = []
     document.getElementById("result").style.gridTemplateRows = "1fr 1fr ".repeat(l.length)
     console.log(document.getElementById("result").style.gridTemplateRows)
     s += `<div class="background-path-line none" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: ${i * 2 + 2}; grid-row-end: ${i * 2 + 3}"></div>`
+    var time = 0
+    SIZE_CONST = 0.1
+
     for (var i = 0; i < (l.length - 1); i++) {
         var from = l[i]
-        var to = l[i + 1]
-        SIZE_CONST = 0.1
-        fracs.push(l[l.length - 1 - i][2] * SIZE_CONST)
-        fracs.push(l[l.length - 1 - i][2] * SIZE_CONST)
+        var to = l[i +1]
+        console.log(from)
+        if (i != 0) {
+            if (to[2] == undefined) {
+                time = time + (to[3] / MAP_SCALE) * SIZE_CONST * 10 // magic constant, it just works    
+            } else {
+             
+            time = time + (to[2] / MAP_SCALE) * SIZE_CONST * 10 // magic constant, it just works
+            }
+        }
+        console.log(time)
+        let times = (Math.floor(time) + "").toHHMMSS()
+        if (i != (l.length - 2)) {
+            let size = Math.floor((to[2] / MAP_SCALE) * SIZE_CONST * 10)
+            if (size < 16) { // Min size
+                size = 16 
+            }
+            fracs.push(size)
+            fracs.push(size)
+        } else {
+            let size = 16
+
+            fracs.push(size)
+            fracs.push(size)
+        }
         if (i == 0) {
-            s += `<div class="background-path-line ${from[1]}" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: ${i * 2 + 2}; grid-row-end: ${i * 2 + 3}"></div>`
+            s += `<div class="background-path-line ${from[1]}" style="background-color: ${lineAttrs[to[1]].stroke};grid-column-start: 1; grid-column-end: 2; grid-row-start: ${i * 2 + 1}; grid-row-end: ${i * 2 + 3}"></div>`
 
         } else {
-            s += `<div class="background-path-line ${from[1]}" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: ${i * 2 + 1}; grid-row-end: ${i * 2 + 3}"></div>`
+            s += `<div class="background-path-line ${from[1]}" style="background-color: ${lineAttrs[to[1]].stroke};grid-column-start: 1; grid-column-end: 2; grid-row-start: ${i * 2 + 0}; grid-row-end: ${i * 2 + 2}"></div>`
         }
-        s += `<div clss="path-item" style="grid-column-start: 2; grid-column-end: 3; grid-row-start: ${i * 2 + 2}; grid-row-end: ${i * 2 + 4}">${to[0]}</div>`
+        s += `<div class="path-item" style="grid-column-start: 2; grid-column-end: 3; grid-row-start: ${i * 2 + 1}; grid-row-end: ${i * 2 + 3}">${to[0]}: ${times}</div>`
         
-    }
-    for (var i = 0; i < l.length; i++) {
-        l[i] = l[i][0]
-    }
-    fracs = fracs.slice(2)
-    total = 0
-    for (var i = 0; i < fracs.length; i++) {
-        total += fracs[i]
-    }
-    console.log(total)
-    factor = 90 / total
-    for (var i = 0; i < fracs.length; i++) {
-        fracs[i] = fracs[i] * factor
     }
 
     document.getElementById("result").innerHTML = s
-    console.log(fracs)
-    document.getElementById("result").style.gridTemplateRows = fracs.join('% ') + "%"
-    console.log(document.getElementById("result").style.gridTemplateRows)
+    console.log(fracs.join('px ') + "px")
+    document.getElementById("result").style.gridTemplateRows = fracs.join('px ') + "px"
     l.splice(0, 0, from)
     for (var i = 0; i < (l.length - 1); i++) {
         Object.keys(lineSeries).forEach(function(k) {
             lineSeries[k].mapLines.each(function (item) {
                 if ((item.route[0].tooltipText == l[i] && item.route[1].tooltipText == l[i + 1]) || (item.route[1].tooltipText == l[i] && item.route[0].tooltipText == l[i + 1])) {
-                    console.log(item.id)
                     item.line.strokeDasharray = ""
                 }
             })
@@ -72,13 +98,38 @@ function clear_path() {
 
 function control_open() {
   document.getElementById("control-panel").style.display = "block";
-  document.getElementById("control-show").style.display = "none";
+  document.getElementById("show-buttons").style.display = "none";
 
 }
 
 function control_close() {
   document.getElementById("control-panel").style.display = "none";
-  document.getElementById("control-show").style.display = "block";
+  document.getElementById("show-buttons").style.display = "block";
+
+  document.body.style.gridTemplateColumns = "2fr 0fr"
+}
+
+function legend_open() {
+  document.getElementById("legend-panel").style.display = "block";
+  document.getElementById("show-buttons").style.display = "none";
+
+}
+
+function legend_close() {
+  document.getElementById("legend-panel").style.display = "none";
+  document.getElementById("show-buttons").style.display = "block";
+
+  document.body.style.gridTemplateColumns = "2fr 0fr"
+}
+function addroad_open() {
+  document.getElementById("addroad-panel").style.display = "block";
+  document.getElementById("show-buttons").style.display = "none";
+
+}
+
+function addroad_close() {
+  document.getElementById("addroad-panel").style.display = "none";
+  document.getElementById("show-buttons").style.display = "block";
 
   document.body.style.gridTemplateColumns = "2fr 0fr"
 }
